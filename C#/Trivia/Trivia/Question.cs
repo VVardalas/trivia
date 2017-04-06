689d3ad5003d3227e28afe28c2b06d3e7e2f7f5e
VARDALAS VINCENT p1611561 <p1611561@univ-lyon1.fr>
2017-04-06 15:32:35 +0200

    classes question et typequestion

---
+++ C#/Trivia/Trivia/Question.cs
@@ -0,0 +1,26 @@
+﻿namespace Trivia
+{
+    public class Question
+    {
+        /*Property*/
+
+        public string Enonce { get; private set; }
+
+        public string Reponse { get; set; }
+
+        public TypeQuestion TypeQuestion { get; private set; }
+
+        /*Constructeur*/
+        public Question(TypeQuestion typeQuestion, string enonce, string reponse)
+        {
+            TypeQuestion = typeQuestion;
+            Enonce = enonce;
+            Reponse = reponse;
+        }
+
+        public override string ToString()
+        {
+            return "La question posée est : " + Enonce;
+        }
+    }
+}
---
+++ C#/Trivia/Trivia/TypeQuestion.cs
@@ -0,0 +1,15 @@
+﻿namespace Trivia
+{
+    public class TypeQuestion
+    {
+        /*Property*/
+        public string Type { get; private set; }
+
+
+        /*Constructeur*/
+        public TypeQuestion(string type)
+        {
+            Type = type;
+        }
+    }
+}
